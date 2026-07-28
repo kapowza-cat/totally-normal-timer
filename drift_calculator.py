a5b056218325e1e3ee294d9a35c6f5c3c9c07d6b
Ryan Ellis <ryan.a.ellis7@gmail.com>
2026-07-28 16:11:51 -0600

    improved antidrift

--- drift_calculator.py
+++ drift_calculator.py
@@ -5,9 +5,14 @@ test_time = 300
 
 
 start = time.perf_counter()
-for count in range(test_time):
-    time.sleep(1)
-end = time.perf_counter()
 
+next_tick = time.perf_counter() + 1
+for count in range(test_time):
+    remaining = next_tick - time.perf_counter()
+    if remaining < 0.9:
+        remaining = 1
+    time.sleep(remaining)
+
+end = time.perf_counter()
 elapsed = end - start
 print(f'Drifted by {abs(test_time-elapsed)}')
